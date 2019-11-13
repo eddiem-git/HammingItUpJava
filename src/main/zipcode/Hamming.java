9930e087eb3c553fd86a20a9f84cab3350772eab
eddie <eddiem9657@gmail.com>
2019-11-13 16:11:53 -0500

    HammerTime

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,26 @@
 package zipcode;
 
 public class Hamming {
+    private String s;
+    private String s1;
+
     public Hamming(String s, String s1) {
+        if (s.length() != s1.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        this.s = s;
+        this.s1 = s1;
     }
 
-    public int getHammingDistance() {
-        return -1;
+    int getHammingDistance() {
+        int sum = 0;
+        for (int currentIndex = 0; currentIndex < this.s.length(); currentIndex++) {
+            if (this.s.charAt(currentIndex) != this.s1.charAt(currentIndex)) {
+                sum++;
+            }
+        }
+        System.out.println(sum);
+        return sum;
     }
+
 }
